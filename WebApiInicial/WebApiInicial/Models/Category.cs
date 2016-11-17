866ee181c8264ce784f628d785216f5e243a4977
claudio.melendres <claudiomelendres@gmail.com>
2016-11-17 00:51:56 -0400

    Add category

---
+++ WebApiInicial/WebApiInicial/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiInicial.Models
+{
+    public class Category
+    {
+        int ID;
+        string Description;
+    }
+}
